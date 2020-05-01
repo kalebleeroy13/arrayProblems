e636cc3b0a9dd7cf94ef621e9dc6a20f19c0fc2c
khuunter@yahoo.com <khuunter@yahoo.com>
2020-05-01 13:48:54 -0400

    reverse array added

--- arrayProbs.js
+++ arrayProbs.js
@@ -1,1 +1,6 @@
-alert("hello mate")
+function reverseArray(arr){
+  // loops through array from end to begining & console logs each element
+  for(let i = arr.length -1; i >= 0; i--){
+    console.log(arr[i]);
+  }
+}
